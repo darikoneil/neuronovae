a947d88c3239a5e38d47f55bdf15321dd3fd0b71
Darik A O'Neil <doneil@salk.edu>
2025-12-19 15:01:31 -0800

    init_dev

--- neuronovae/__init__.py
+++
@@ -1,7 +0,0 @@
-from neuronovae.constants import Constants
-from neuronovae.io_tools import load_image
-
-__all__ = [
-    "Constants",
-    "load_image",
-]
--- neuronovae/hints.py
+++
@@ -1,9 +0,0 @@
-from os import PathLike
-from pathlib import Path
-
-"""
-This module contains some custom aliases for type-hinting
-"""
-
-#: A valid file path as a string, Path or PathLike object
-File = str | Path | PathLike
